57ba80a54b146d8cd3e619b19742490efb844219
Teddy55 <sven.wegmueller.kt55@gmail.com>
2022-04-09 19:54:48 +0200

    added functionalitiy for playlist option

--- main.py
+++ main.py
@@ -10,7 +10,9 @@ import unicodedata
 
 from PIL import Image, ImageTk
 from pydub import AudioSegment
+from pytube import Playlist
 from pytube import YouTube
+
 from constants import *
 
 with open("resources/loading_messages.json", encoding="UTF-8") as f:
@@ -30,7 +32,9 @@ try:
 except Exception:
     pass
 
-download_mp3 = True
+toggle_amount_button = None
+selected_mp3 = True
+selected_video = True
 download_video = True
 
 img = ImageTk.PhotoImage(Image.open("resources/Huntergruden.png"))
@@ -75,60 +79,98 @@ def loading_animation():
         time.sleep(3)
 
 
-def download_audio():
+def handle_download_video(url, playlist=None):
+    video = YouTube(url)
+
+    if selected_mp3:
+        file_name = download_mp3(video, playlist)
+    else:
+        file_name = download_mp4(video, playlist)
+
+    return file_name
+
+
+def download_mp3(video, playlist):
+    stream = video.streams.get_audio_only()
+
+    file_name = stream.default_filename[:-4]
+
+    stream.download(output_path=f"./temp/")
+
+    given_audio = AudioSegment.from_file(f"./temp/{stream.default_filename}", format="mp4")
+
+    if playlist:
+
+        if not os.path.exists(f"./output/mp3/{playlist}"):
+            os.makedirs(f"./output/mp3/{playlist}")
+
+        given_audio.export(f"./output/mp3/{playlist}/{file_name}.mp3", format="mp3")
+    else:
+        given_audio.export(f"./output/mp3/{file_name}.mp3", format="mp3")
+
+    shutil.rmtree("temp")
+    os.makedirs("temp")
+
+    return file_name
+
+
+def download_mp4(video, playlist):
+    stream = video.streams.get_highest_resolution()
+
+    file_name = stream.default_filename[:-4]
+
+    if playlist:
+        stream.download(output_path=f"./output/mp4/{playlist}")
+    else:
+        stream.download(output_path=f"./output/mp4")
+
+    return file_name
+
+
+def setup_download():
     global exited
     exited = False
 
     t2 = threading.Thread(target=loading_animation)
     t2.start()
 
-    url1 = url_input.get()
-    yt = YouTube(url1)
+    url_from_input = url_input.get()
 
-    if download_mp3:
+    if not selected_video:
+        playlist = Playlist(url_from_input)
 
-        stream = yt.streams.get_audio_only()
-
-        file_name = stream.default_filename[:-4]
-
-        stream.download(output_path=f"./temp/")
-
-        given_audio = AudioSegment.from_file(f"./temp/{stream.default_filename}", format="mp4")
-
-        given_audio.export(f"./output/mp3/{file_name}.mp3", format="mp3")
-
-        shutil.rmtree("temp")
-        os.makedirs("temp")
+        if len(playlist.video_urls) != 0:
+            for video in playlist.video_urls:
+                handle_download_video(video, playlist.title)
+            output_name = playlist.title
+        else:
+            output_name = handle_download_video(url_from_input)
 
     else:
-        stream = yt.streams.get_highest_resolution()
-
-        file_name = stream.default_filename[:-4]
-
-        stream.download(output_path=f"./output/mp4")
+        output_name = handle_download_video(url_from_input)
 
     exited = True
 
-    display_name = file_name
-    for counter, character in enumerate(file_name):
+    display_name = output_name
+    for counter, character in enumerate(output_name):
         if counter % 35 == 0:
             display_name = display_name[:counter] + "\n" + display_name[counter:]
     output_label["text"] = f"downlaoded {display_name}"
 
 
-def start_download_audio():
-    t1 = threading.Thread(target=download_audio)
+def starting_setup_download():
+    t1 = threading.Thread(target=setup_download)
     t1.start()
 
 
 def toggle_mp3_mp4():
-    global download_mp3, lever_clicks, toggle_amount_button
+    global selected_mp3, lever_clicks, toggle_amount_button
 
-    if download_mp3:
+    if selected_mp3:
         toggle_format_button["image"] = right_btn_img
     else:
         toggle_format_button["image"] = left_btn_img
-    download_mp3 = not download_mp3
+    selected_mp3 = not selected_mp3
 
     lever_clicks += 1
     if lever_clicks == CLICKS_TO_UNLOCK:
@@ -140,13 +182,12 @@ def toggle_mp3_mp4():
 
 
 def toggle_video_playlist():
-    global download_video, toggle_amount_button
-
+    global selected_video
     if download_video:
         toggle_amount_button["image"] = right_btn_img
     else:
         toggle_amount_button["image"] = left_btn_img
-    download_video = not download_video
+    selected_video = not selected_video
 
 
 btn_img = tk.PhotoImage(file="./resources/downlaod.png")
@@ -155,7 +196,7 @@ right_btn_img = tk.PhotoImage(file="resources/right.png")
 
 left_btn_img = tk.PhotoImage(file="resources/left.png")
 
-entry_button = tk.Button(canvas, command=start_download_audio, image=btn_img)
+entry_button = tk.Button(canvas, command=starting_setup_download, image=btn_img)
 entry_button.pack()
 
 output_label = tk.Label(canvas, font=best_font, bg="#72D0FF", fg="pink")
