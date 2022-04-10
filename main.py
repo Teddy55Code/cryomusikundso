7fc9fd285041308cecd0e895dc92972f60bb1ca9
Teddy55 <sven.wegmueller.kt55@gmail.com>
2022-04-10 05:52:05 +0200

    removed slugify function (not used)

--- main.py
+++ main.py
@@ -1,12 +1,10 @@
 import json
 import os
 import random
-import re
 import shutil
 import threading
 import time
 import tkinter as tk
-import unicodedata
 
 import music_tag
 import requests
@@ -55,23 +53,6 @@ url_input.pack()
 
 bottom_uwu_padding_for_input = tk.LabelFrame(canvas, bg="#72D0FF", height=60)
 bottom_uwu_padding_for_input.pack()
-
-
-def slugify(value, allow_unicode=False):
-    """
-    Taken from https://github.com/django/django/blob/master/django/utils/text.py
-    Convert to ASCII if 'allow_unicode' is False. Convert spaces or repeated
-    dashes to single dashes. Remove characters that aren't alphanumerics,
-    underscores, or hyphens. Convert to lowercase. Also strip leading and
-    trailing whitespace, dashes, and underscores.
-    """
-    value = str(value)
-    if allow_unicode:
-        value = unicodedata.normalize("NFKC", value)
-    else:
-        value = unicodedata.normalize("NFKD", value).encode("ascii", "ignore").decode("ascii")
-    value = re.sub(r"[^\w\s-]", "", value.lower())
-    return re.sub(r"[-\s]+", "-", value).strip("-_")
 
 
 def loading_animation():
